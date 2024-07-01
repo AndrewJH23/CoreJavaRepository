30d8bc14f4cde48944be0f0db4e2ced832f0e108
AndrewJH23 <andrew.j.hom@gmail.com>
2024-07-01 16:48:54 -0400

    first commit

---
+++ src/main/java/com/example/demo/SpringJenkinsApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+
+import org.slf4j.*;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+	
+	public static Logger log = LoggerFactory.getLogger(SpringJenkinsApplication.class); 
+	
+	public void init() {
+		log.info("Application started...");
+	}
+	
+	public static void main(String[] args) {
+		log.info("Application executed");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
